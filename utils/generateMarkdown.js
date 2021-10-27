d9de95224c5a704fc61300e90eb3ed6f6fde7c92
Nsmall <nsmall1994@gmail.com>
2021-10-27 19:31:03 -0400

    additions/changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = license => {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
